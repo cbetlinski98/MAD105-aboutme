140a865fa06d0fa4d1a802f014547a6561ef6d54
cbetlinski98 <cbetlinski@students.mchenry.edu>
2018-09-01 23:15:53 -0500

    Adding aboutme file
    A short program using println statements.

---
+++ aboutme/src/com/example/java/Main.java
@@ -0,0 +1,12 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//Using println
+        System.out.println("Hi! My name is Codie!");
+        System.out.println("I enjoy playing video games. I have been a gamer since I could remember.");
+        System.out.println("I do not have any preference when it comes to what I game on.");
+        System.out.println("I am interested in joining the game design field.");
+    }
+}
